0a11a8dfd7fe4169921b873d532587c13764c79b
王宁 <2556405022@qq.com>
2018-09-10 09:17:47 +0800

    dd
    第一次改动

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace testApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("第一次写Cplusplusplusplus");
         }
     }
 }
